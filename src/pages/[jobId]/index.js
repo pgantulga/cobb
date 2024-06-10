43ac95827bbd176454f793a593ae29b24827bfea
pgantulga <pggan2@gmail.com>
2024-06-10 11:05:32 +1000

    latest

--- src/pages/[jobId]/index.js
+++ src/pages/[jobId]/index.js
@@ -2,59 +2,59 @@ import JobDetail from '@/components/feature/jobs/JobDetail/JobDetail';
 import Head from 'next/head';
 
 function JobDetailPage(props) {
-    // const { job } = props;
+    const { job } = props;
 
     return (
         <>
             <Head>
-                {/* <title>{`Cobb | ${job.name}`}</title> */}
+                <title>{`Cobb | ${job.name}`}</title>
                 <meta
                     name='description'
-                    // description={job.description}
+                    description={job.description}
                 />
             </Head>
-            {/* <JobDetail
+            <JobDetail
                 imageUrl={job.imageUrl}
                 name={job.name}
                 description={job.description}
                 location={job.location}
-            /> */}
+            />
         </>
     );
 }
 
-// export const getStaticPaths = async () => {
+export const getStaticPaths = async () => {
 
-//     const response = await fetch(`${process.env.SERVER_NAME}/api/jobs`);
-//     const jobs = await response.json();
+    const response = await fetch(`${process.env.SERVER_NAME}/api/jobs`);
+    const jobs = await response.json();
 
-//     const idList = jobs.map((job) => job.id);
+    const idList = jobs.map((job) => job.id);
 
-//     const paths = idList.map((id) => (
-//         { params: { jobId: id.toString() } }
-//     ));
+    const paths = idList.map((id) => (
+        { params: { jobId: id.toString() } }
+    ));
 
-//     return {
-//         paths,
-//         fallback: false
-//     }
-// }
+    return {
+        paths,
+        fallback: false
+    }
+}
 
-// export const getStaticProps = async (context) => {
-//     const response = await fetch(`${process.env.SERVER_NAME}/api/jobs`);
-//     const jobs = await response.json();
+export const getStaticProps = async (context) => {
+    const response = await fetch(`${process.env.SERVER_NAME}/api/jobs`);
+    const jobs = await response.json();
 
-//     const jobQuery = context.params.jobId;
+    const jobQuery = context.params.jobId;
 
-//     const jobMatch = jobs.filter(
-//         (job) => job.id.toString() === jobQuery
-//     )
+    const jobMatch = jobs.filter(
+        (job) => job.id.toString() === jobQuery
+    )
 
-//     return {
-//         props: {
-//             job: jobMatch[0]
-//         },
-//     };
-// };
+    return {
+        props: {
+            job: jobMatch[0]
+        },
+    };
+};
 
 export default JobDetailPage;
