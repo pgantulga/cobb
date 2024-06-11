d0dc999b9213da2475c8e41389b125b0a9099dee
pgantulga <pggan2@gmail.com>
2024-06-11 20:51:41 +1000

    ga-install

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,27 @@
 import "@/styles/globals.css";
 import Layout from "@/components/layout";
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import Head from "next/head";
 import Script from "next/script";
+import { useRouter } from 'next/router'
+
+import * as ga from "../lib/google-analytics"
 
 export default function App({ Component, pageProps }) {
+  // C. SETUP OF GA PAGE VIEWS
+  const router = useRouter();
+  useEffect(() => {
+    // C1: Declare function which passes clicked page url to GA4 "event" config function
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    // C2: Subscribe to the change event when component mounts
+    router.events.on('routeChangeComplete', handleRouteChange)
+    // C3: Unsubscribe from change event on unmount
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return (
     <Fragment>
       <Script
